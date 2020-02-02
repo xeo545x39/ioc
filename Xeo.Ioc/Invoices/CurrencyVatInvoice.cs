cc7cd1831873a66a20a105d96bd3cb1fb2cfb09b
Xeo <xeo545x39@gmail.com>
2020-02-02 14:58:51 +0100

    TASK1 ANSWERS

--- Xeo.Ioc/Invoices/CurrencyVatInvoice.cs
+++ Xeo.Ioc/Invoices/CurrencyVatInvoice.cs
@@ -13,24 +13,37 @@ namespace Xeo.Ioc.Invoices
         public CurrencyVatInvoice(
             decimal net, 
             decimal taxRate, 
+            // zrobić z currencySymbol klasę, która waliduje symbol
             string currencySymbol) 
             : base(net, taxRate)
             => _currencySymbol = currencySymbol;
 
-        public async Task<decimal> GetValue()
+        // dodać GetValueAsync do interfejsu, usunąć synchroniczną
+        public override decimal GetValue()
+            => GetValueAsync()
+                .GetAwaiter()
+                .GetResult();
+
+        public async Task<decimal> GetValueAsync()
         {
+            // 1. HttpClient nie może być tworzony w ten sposób, ponieważ będzie tworzył wycieki. Trzeba cacheować, lub korzystać z IHttpClientFactory
+            // 2. Wyekstraktować logikę na zewnątrz
             var response = await new HttpClient().GetAsync($"https://nbp.pl/rates/{_currencySymbol}/pln");
 
             if (response.IsSuccessStatusCode)
             {
-                await using Stream stream 
+                // brak zamknięcia strumienia, wycieki
+                Stream stream 
                     = await response.Content.ReadAsStreamAsync();
                 decimal exchangeRate 
                     = Convert.ToDecimal(await JsonSerializer.DeserializeAsync<string>(stream));
 
+                // brak walidacji danych przychodzących
+                
                 return base.GetValue() * exchangeRate;
             }
             
+            // błąd zbyt ogólny, dodać klasę wyjątku ExchangeRateLoadingException
             throw new Exception("Could not get the exchange rate.");
         }
     }
